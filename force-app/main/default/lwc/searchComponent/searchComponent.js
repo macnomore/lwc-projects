851f14c46292d5525f66dc0ca3d815762e810540
Ian Mathenge <imathenge@blueconsulting.co.ke>
2024-08-23 23:08:45 +0300

    Create search component

---
+++ force-app/main/default/lwc/searchComponent/searchComponent.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from "lwc";
+
+export default class SearchComponent extends LightningElement {
+  @track searchKey;
+  handleChange(event) {
+    const searchKey = event.target.value;
+    event.preventDefault();
+    const searchEvent = new CustomEvent("change", {
+      detail: searchKey
+    });
+    this.dispatchEvent(searchEvent);
+  }
+}
